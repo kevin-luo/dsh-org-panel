06baeffadeac9db8142cc101a3a1a0bcd3e8a7a7
kevin-luo <166820145@qq.com>
2026-08-17 11:23:39 +0800

    feat: enable immersive headquarters v6

--- src/client.tsx
+++ src/client.tsx
@@ -1,4 +1,4 @@
 // 「赛博公司」DSH 插件 client 入口
-// v5：AI 员工总部控制台 + 扩展员工名册 + 兼容现有组织分组。
+// v6：沉浸式 AI 员工总部 + 扩展员工 + 成长系统 + 社区插件雷达。
 
-export { apply } from './client-v5'
+export { apply } from './client-v6'
